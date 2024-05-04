87a380d5e6db4e15f7f694c5a217502f6c6d7396
MoAdel <mohamedadelali2003@gmail.com>
2024-05-04 22:25:39 +0300

    Add delete multiple tasks and updating localStorage functionalities

--- main.js
+++ main.js
@@ -8,23 +8,48 @@ import {
 
 // Messages
 const mainMsg = "1-Add a task\n2-Search for a task\n3-Delete a task\n4-Display tasks"
-const deleteMsg = "1-delete first task\n2-delete last task\n3-delete by index\n4-delete by name\n5-delete from x to y tasks";
+const deleteMsg = "1-delete first task\n2-delete last task\n3-delete by index\n4-delete by name\n5-delete from x to y tasks\n6-delete multiple tasks";
 
 let testArr = ["t1","t2","t3","t4"];
 
 
+function updateLocalStorage(){
+    localStorage.setItem("tasks",testArr)
+}
+
+function getTasksFromStorage() {
+    let stored = localStorage.getItem("last_update");
+    
+    if (stored) {
+        let tasks = stored.split(',');
+        return tasks;
+    } else {
+        return [];
+    }
+}
+
+
+
+function separateString(string) {
+    var array = string.split(',');
+    return array;
+}
+
 function DisplayMenu(){
     let input = prompt(mainMsg);
-    if(input == null || input == ""){
+    if(input == null ){
         receivingInput = false
     }else{
         if(input == 3){
             let deleteInput = prompt(deleteMsg);
             // console.log(deleteInput);
+           
 
             if(deleteInput == 1){
                 deleteFirstTask(testArr);
                 console.log(testArr);
+                
+               
             }
             if(deleteInput == 2){
                 deleteLastTask(testArr);
@@ -46,6 +71,18 @@ function DisplayMenu(){
                 deleteFromXToY(parseInt(delX),parseInt(delY),testArr);
                 console.log(testArr);
             }
+            if(deleteInput == 6){
+                let multiInput = prompt("Please provide the indices of the desired tasks to delete as this format [task1,task2,....]")
+                let indices = separateString(multiInput);
+                for(let i = 0;i<indices.length;i++){
+                    deleteWithIndex(parseInt(indices[i]),testArr,)
+                }
+                console.log(testArr);
+
+            }
+
+            updateLocalStorage()
+            console.log(getTasksFromStorage());
 
         }
     }
